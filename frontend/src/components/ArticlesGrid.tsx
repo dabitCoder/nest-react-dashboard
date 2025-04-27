e3eef3288800f33671c471dddef872a82acd0140
David Oliva <doliva@icopartners.com>
2025-04-27 18:51:52 +0200

    Frontend - Articles Grid

--- frontend/src/components/ArticlesGrid.tsx
+++ frontend/src/components/ArticlesGrid.tsx
@@ -1,12 +1,13 @@
-import {
-  useCallback,
-  useEffect,
-  useState,
-  useTransition,
-} from "react";
+import { useCallback, useEffect, useState, useTransition } from "react";
 import { fetchArticles } from "../services/api.ts";
 import Spinner from "./Spinner.tsx";
-import {  ArticlesResponse } from "../types.ts";
+import {ArticleQueryParams, ArticlesResponse} from "../types.ts";
+import ArticleCard from "./ArticleCard.tsx";
+
+const fetchParams: ArticleQueryParams = {
+	page: 1,
+	limit: 10,
+}
 
 const ArticlesGrid = () => {
   const [articles, setArticles] = useState<ArticlesResponse>({
@@ -15,12 +16,12 @@ const ArticlesGrid = () => {
   });
 
   const [isPending, startTransition] = useTransition();
-  const [error, setError] = useState<Error | unknown>(null);
+  const [error, setError] = useState<Error>(null);
 
   const fetchData = useCallback(() => {
     startTransition(async () => {
       try {
-        const response: ArticlesResponse = await fetchArticles();
+        const response: ArticlesResponse = await fetchArticles(fetchParams);
         setArticles(response);
       } catch (error) {
         console.error("Error fetching articles:", error);
@@ -46,17 +47,22 @@ const ArticlesGrid = () => {
   }
 
   return (
-    <section className="mt-6">
+    <section className="mt-14">
       <div className="flex flex-col md:justify-between md:flex-row gap-2 mb-4">
         <h2 className="text-xl font-semibold text-gray-800">
           All Articles ({articles.total} in total)
         </h2>
-				<input
-					type="text"
-					placeholder="Search articles..."
-					className="w-64 p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
-				/>
+        <input
+          type="text"
+          placeholder="Search articles..."
+          className="w-64 p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+        />
       </div>
+			<div className="flex flex-col md:grid md:grid-cols-3 gap-2 md:gap-6 mb-4 ">
+      {articles.data.length
+        ? articles.data.map((article) => <ArticleCard article={article} />)
+        : null}
+			</div>
     </section>
   );
 };
